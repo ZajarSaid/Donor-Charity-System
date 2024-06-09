50587c4f7913416192a1a847cf400eb04dc07faa
ZajarSaid <zajarjafary@gmail.com>
2024-06-10 01:37:42 +0300

    added a conversation and conversation message models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from users.models import CustomUser
 # Create your models here.
+
+class Conversation(models.Model):
+    members = models.ManyToManyField(CustomUser, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.members
+
+    class Meta:
+        verbose_name_plural = 'Conversation'
+        ordering = ('-modified_at',)
+
+
+class ConversationMessage(models.Model):
+    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser, related_name='user_messages', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.conversation
